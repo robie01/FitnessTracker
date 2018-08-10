4bd119518621a5bc78c0ed76954ab4dfadb36091
robie01 <robie.sun.rs@gmail.com>
2018-08-10 10:39:01 +0200

    example for two way binding: property and event binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import {ProductsComponent} from './products.component';
 import { ProductComponent } from './product/product.component';
 import {RouterModule, Routes} from "@angular/router";
+import {FormsModule} from "@angular/forms";
 
 const routes: Routes = [
   {path: 'product', component: ProductComponent}
@@ -18,7 +19,8 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
