4fed99e984c05ab920a2b909b8732fc5c5832ab9
robie01 <robie.sun.rs@gmail.com>
2018-08-10 14:49:02 +0200

    using form tag for multiple inputs

--- src/app/products.component.ts
+++ src/app/products.component.ts
@@ -15,8 +15,11 @@ export class ProductsComponent {
     }, 3000);
   }
 
-  onAddProduct() {
-    this.products.push(this.productName);
+  onAddProduct(form) {
+    // this.products.push(this.productName);
+    if (form.valid) {
+      this.products.push(form.value.productName);
+    }
 
   }
   onRemoveProduct(productName: string) {
